f066177bcd8dfaae9a4784865e53a380d185838c
Andy McWilliams <andrew.mcwilliams95@gmail.com>
2017-07-26 21:29:09 +0100

    Added a second activity

--- app/src/main/java/com/andymcwilliams/androidproject/MainActivity.java
+++ app/src/main/java/com/andymcwilliams/androidproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.andymcwilliams.androidproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Opens activity 2
+    public void openActivity2(View view) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
     }
 }
